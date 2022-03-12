d44e1cfd3552914c30d14e97e5ba4f7a823da259
Baturpcu <batusalkim11@gmail.com>
2022-03-12 23:10:49 +0300

    internet programcılığı dersi
    internet programcılığı dersi çalışmaları

---
+++ uyg02.1/uyg02.1/Controllers/ServisController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using uyg02._1.Models;
+using uyg02._1.ViewModel;
+
+namespace uyg02._1.Controllers
+{
+    public class ServisController : ApiController
+    {
+        DB01Entities db = new DB01Entities();
+
+
+        [HttpGet]
+        [Route("api/ogrenciliste")]
+
+        public  List<OgrenciModel> OgrenciListe()
+        {
+            List<OgrenciModel> liste = db.Ogrenci.Select(x => new OgrenciModel()
+            {
+                ogrId = x.ogrId,
+                ogrNo = x.ogrNo,
+                ogrAdsoyad = x.ogrAdsoyad,
+                ogrMail = x.ogrMail,
+                ogrYas = x.ogrYas
+            }).ToList();
+   
+            return liste;
+        }
+
+
+        [HttpGet]
+        [Route("api/ogrencibyid/{ogrId}")]
+        public OgrenciModel OgrenciById(int ogrId)
+        {
+            OgrenciModel kayit = db.Ogrenci.Where(s => s.ogrId == ogrId).Select
+                (x => new OgrenciModel()
+                {
+                    ogrId = x.ogrId,
+                    ogrNo = x.ogrNo,
+                    ogrAdsoyad = x.ogrAdsoyad,
+                    ogrMail = x.ogrMail,
+                    ogrYas = x.ogrYas
+                }).SingleOrDefault();
+
+            return kayit;
+        }
+    }
+}
---
+++ uyg02.1/uyg02.1/ViewModel/OgrenciModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace uyg02._1.ViewModel
+{
+    public class OgrenciModel
+    {
+        public int ogrId { get; set; }
+        public string ogrNo { get; set; }
+        public string ogrAdsoyad { get; set; }
+        public string ogrMail { get; set; }
+        public int ogrYas { get; set; }
+    }
+}
